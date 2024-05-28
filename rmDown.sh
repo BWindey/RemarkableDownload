ae621d8ff0779602a2499ed6fbcab1562f2b8542
Bram Windey <windey.bram@gmail.com>
2024-05-28 21:16:08 +0200

    Fix bash stuff right quotes and stuff

--- rmDown.sh
+++ rmDown.sh
@@ -19,7 +19,8 @@ mkdir "${1}_rm"
 
 for entry in "${foundDirectories[@]}"; do
     id="$(basename ${entry%.metadata})"
-    echo "$id"
+    echo "Directory id: ${id}"
+
     IFS=$'\n' read -rd '' -a foundFiles <<< \
         "$(ssh root@10.11.99.1 bash -s << EOF
 for file in \$(grep -li "parent\": \"${id}\"" /home/root/.local/share/remarkable/xochitl/*.metadata); do
@@ -31,7 +32,7 @@ EOF
 )"
     for file in "${foundFiles[@]}"; do
         fileId="$(basename ${file%.metadata})"
-        fileName="$(ssh root@10.11.99.1 \"grep 'visibleName' ${file}\" | sed 's/.*"\([^"]*\)"[, ]*$/\1/')"
+        fileName=$(ssh root@10.11.99.1 "grep 'visibleName' ${file}" | sed 's/.*"\([^"]*\)"[, ]*$/\1/')
         echo -e "\e[1m${fileName}:\e[0;3m ${fileId}\e[0m"
         wget -q -O "${1}_rm/${fileName}.pdf" "http://10.11.99.1/download/${fileId}/placeholder"
         test $? == 0 && echo "  downloaded"
